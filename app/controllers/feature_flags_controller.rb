a12a105a15752686ae351fe883a8ed1a5dbfba31
Rabin Gaire <rabingaire20@gmail.com>
2018-03-10 08:51:00 +0545

    added feature flag model and controller

---
+++ app/controllers/feature_flags_controller.rb
@@ -0,0 +1,2 @@
+class FeatureFlagsController < ApplicationController
+end
---
+++ test/controllers/feature_flags_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FeatureFlagsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
