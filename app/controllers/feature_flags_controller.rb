a898ffb2468239e8cdeee21e91b186541dce7552
Rabin Gaire <rabingaire20@gmail.com>
2018-03-10 08:58:33 +0545

    added index page

--- app/controllers/feature_flags_controller.rb
+++ app/controllers/feature_flags_controller.rb
@@ -1,2 +1,5 @@
 class FeatureFlagsController < ApplicationController
+  def index
+
+  end
 end
--- db/migrate/20180310030532_create_feature_flags.rb
+++
@@ -1,8 +0,0 @@
-class CreateFeatureFlags < ActiveRecord::Migration[5.1]
-  def change
-    create_table :feature_flags do |t|
-
-      t.timestamps
-    end
-  end
-end
